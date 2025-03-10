b28c6badb77243eca86b3e3f162b9a2d70e8832d
YojanErazo <yojanerazo07@gmail.com>
2025-03-10 11:02:30 -0600

    Se agrego la carpeta infra y unos cambios en el backend

---
+++ Backend/API/Configuration/DatabaseConfig.cs
@@ -0,0 +1,28 @@
+﻿using DotNetEnv;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Configuration
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; }
+
+        public DatabaseConfig()
+        {
+            DotNetEnv.Env.Load();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            string dbServer = configuration["DB_SERVER"] ?? "localhost";
+            string dbPort = configuration["DB_PORT"] ?? "1234";
+            string dbName = configuration["DB_NAME"] ?? "base";
+            string dbUser = configuration["DB_USER"] ?? "cons";
+            string dbPassword = configuration["DB_PASSWORD"] ?? "sorto";
+
+            ConnectionString = $"Server={dbServer},{dbPort};Database={dbName};User Id={dbUser};Password={dbPassword};TrustServerCertificate=True;";
+        }
+    }
+}
---
+++ Backend/API/Controllers/BranchController.cs
@@ -0,0 +1,11 @@
+﻿using Aplication.DTOs;
+using Aplication.Interfaces;
+using Domain.Entities;
+
+namespace API.Controllers
+{
+    public class BranchController : CatalogController<Branch, CatalogRequestDTO, CatalogResponseDTO>
+    {
+        public BranchController(IService<Branch, CatalogRequestDTO, CatalogResponseDTO> service) : base(service) { }
+    }
+}
